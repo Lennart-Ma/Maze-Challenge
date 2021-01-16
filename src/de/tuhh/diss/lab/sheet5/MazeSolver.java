c8e319483f82e7beede75a597e36e623367a184d
Lennart-Ma <72752538+Lennart-Ma@users.noreply.github.com>
2021-01-16 12:49:21 +0100

    Update the MazeSolver superclass with solvingMaze() function

--- src/de/tuhh/diss/lab/sheet5/MazeSolver.java
+++ src/de/tuhh/diss/lab/sheet5/MazeSolver.java
@@ -6,11 +6,44 @@ import lejos.utility.Delay;
 
 public class MazeSolver {
 	
-	private String wantedColor;
-	private String foundColor;
-	private static boolean startRobot;
+	private static String wantedColor;
+	private static String foundColor;
+	private static boolean colorFound;
 	private static RobotStarter robotStarter;
+	private static SimpleBeeper simpleBeeper;
+	private static ColorDetector colorDetector;
+	private static GyroTurner turner;
+	private static ForwardDriver driver; 
 	
+	
+	private static boolean solvingMaze() {
+		
+		foundColor = colorDetector.getColor();
+		colorFound = false;
+		
+		if (foundColor == wantedColor) {
+			simpleBeeper.playBeep();
+			colorFound = true;
+		} else {
+			turner.turnCCW();
+			foundColor = colorDetector.getColor();
+			if (foundColor == wantedColor) {
+				simpleBeeper.playBeep();
+				colorFound = true;
+			} else if (foundColor == "None") {
+				driver.driveForward();
+			} else {
+				turner.turnCW();
+				foundColor = colorDetector.getColor();
+				if (foundColor == "None") {
+					driver.driveForward();
+				} else {
+					turner.turnCW();
+				}
+			}
+		}
+		return colorFound;
+	}
 
 	public static void main(String[] args) {
 		MazebotSimulation sim = new MazebotSimulation("Mazes/TestArea.png", 1.8 , 1.45);
@@ -23,8 +56,17 @@ public class MazeSolver {
 		
 		
 		robotStarter = new RobotStarter();
+		simpleBeeper = new SimpleBeeper();
+		colorDetector = new ColorDetector();
 		
 		robotStarter.startRobot();
+		simpleBeeper.playBeep();
+
+		wantedColor = robotStarter.getColor();
+		
+		while (colorFound == false) {
+			colorFound = solvingMaze();
+		}
 		
 		
 		Delay.msDelay(100);
