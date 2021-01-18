06638148359fa7d26bfdd1790279169af2c80b51
Tobias Behne <Tobias Behne@DESKTOP-FJ4V931>
2021-01-18 13:34:11 +0100

    driveForward class now working

--- src/de/tuhh/diss/lab/sheet5/Drive.java
+++ src/de/tuhh/diss/lab/sheet5/Drive.java
@@ -5,23 +5,51 @@ import lejos.utility.Delay;
 
 public class Drive implements Driver{
 	
-	private int angle;
 	private RegulatedMotor leftMotor;
-	private RegulatedMotor rightMotor;	
+	private RegulatedMotor rightMotor;
+	private int degreesPerSecond;
+	private static final int WHEELDIAMETER = 54; //in mm
+	private static final int TILELENGTH = 350; //in mm
+	private static final int ANGULARVELOCITY = 1000;
 	
 	public Drive(RegulatedMotor leftMotor, RegulatedMotor rightMotor) {
-		
-		angle = (int) (35 * 60 / Math.PI * 5.4);
-		this.leftMotor= leftMotor;
+		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;
-		
 	}
 	
-	
+
 	public void driveForward() {
 		
-		rightMotor.rotateTo(angle);
-		leftMotor.rotateTo(angle);
-	}
+		int motorDegree = setMotorDegree();
+		setSpeed();
+		double degreesPerSecond_double = this.degreesPerSecond;
+		double delay_time = (Math.abs(motorDegree)/Math.abs(degreesPerSecond_double))*1000;
+		int delay_time_int = (int)delay_time;
+		
+		Delay.msDelay(2000);
 
+		this.rightMotor.rotate(motorDegree,true);
+		this.leftMotor.rotate(motorDegree,true);
+
+		Delay.msDelay(delay_time_int);
+		System.out.println("Done");
+	}
+	
+	
+	private int setMotorDegree() {
+		
+		double alphaWheel = (TILELENGTH/(Math.PI * WHEELDIAMETER)) *360;
+		double alphaMotor = 3*alphaWheel;
+		int alphaMotorPerMotor = (int)alphaMotor*2;
+		
+		return alphaMotorPerMotor;
+	}
+	
+	
+	public void setSpeed() {
+		this.rightMotor.setSpeed(ANGULARVELOCITY);
+		this.leftMotor.setSpeed(ANGULARVELOCITY);
+	}
 }
+
+
