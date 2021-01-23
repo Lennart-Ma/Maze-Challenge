b0ce51981d7fc86f347600dc2da2d4c14a136a81
Tobias Behne <Tobias Behne@DESKTOP-FJ4V931>
2021-01-23 15:53:20 +0100

    opti

--- src/de/tuhh/diss/lab/sheet5/Drive.java
+++ src/de/tuhh/diss/lab/sheet5/Drive.java
@@ -14,7 +14,7 @@ public class Drive implements Driver{
 	private final int WHEELDIAMETER = 54; //in mm
 	private final int ANGULAR_VELOCITY = 760;
 	private final int TILELENGTH = 350; //in mm
-	private double TILE_LENGTH = 350;
+	private final int DISTANCE_TO_WALL = 65;
 	private double distanceToWallDouble;
 	private int distanceToWall;
 	private double actualDistToWall;
@@ -43,9 +43,9 @@ public class Drive implements Driver{
 	public void approachTileEdge(boolean towards) {
 		
 		if (towards) {
-			distanceToWall = -65;
+			distanceToWall = -DISTANCE_TO_WALL;
 		} else {
-			distanceToWall = 65;
+			distanceToWall = DISTANCE_TO_WALL;
 		}
 		
 		int motorDegree = setMotorDegree(distanceToWall);
@@ -64,7 +64,7 @@ public class Drive implements Driver{
 		
 		actualDistToWall = checkDistance();
 		
-		distanceToWallDouble = ((TILE_LENGTH/2) - (actualDistToWall + 55));
+		distanceToWallDouble = ((TILELENGTH/2) - (actualDistToWall + 55));
 				
 		distanceToWall = (int)distanceToWallDouble;
 				
